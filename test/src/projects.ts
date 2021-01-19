ef5cdb3a18fd482d399ba6c436fd0881118dc74e
Connor Turland <connorturland@gmail.com>
2021-01-19 03:44:16 -0500

    update test

--- test/src/projects.ts
+++ test/src/projects.ts
@@ -32,23 +32,6 @@ async function setup(scenario: ScenarioApi) {
   return { callAlice, agentAddress: agent }
 }
 
-async function runPlainCreateFetchTest({
-  entryType,
-  createEntry,
-  callAlice,
-  tape,
-}) {
-  // CREATE
-  const entry = createEntry
-  const createResult = await callAlice(`create_${entryType}`, entry)
-  tape.deepEqual(createResult.entry, entry)
-
-  // READ
-  const fetchResult = await callAlice(`fetch_${entryType}s`, null)
-  tape.equal(fetchResult.length, 1)
-  tape.deepEqual(fetchResult[0], createResult)
-}
-
 module.exports = (orchestrator: Orchestrator<null>) => {
   orchestrator.registerScenario(
     'trx api',
@@ -58,17 +41,20 @@ module.exports = (orchestrator: Orchestrator<null>) => {
       const { Codec } = await import('@holo-host/cryptolib')
       const aa = Codec.AgentId.encode(agentAddress)
 
-      await runPlainCreateFetchTest({
-        entryType: 'trx',
-        createEntry: {
-          from: aa,
-          to: aa, // TODO: change this to someone elses address
-          created_at: Date.now(),
-          amount: 100,
-        },
-        callAlice,
-        tape,
-      })
+      // TODO: change TO to someone elses address
+      const entry = {
+        from: aa,
+        to: aa, 
+        created_at: Date.now(),
+        amount: 100,
+      }
+      const createResult = await callAlice(`create_trx`, entry)
+      tape.deepEqual(createResult.entry, entry)
+
+      // READ
+      const fetchResult = await callAlice(`fetch_trxs`, null)
+      tape.equal(fetchResult.length, 1)
+      tape.deepEqual(fetchResult[0], createResult)
     }
   )
 
@@ -86,17 +72,16 @@ module.exports = (orchestrator: Orchestrator<null>) => {
         tape.equal(true, e.data.data.includes('no project meta exists'))
       }
 
-      const entryType = 'project_meta'
       // CREATE
       const entry = {
         creator_address: aa,
         created_at: Date.now(),
         passphrase: 'pinky-stomp-tuffle-waffle',
       }
-      const createResult = await callAlice(`create_${entryType}`, entry)
+      const createResult = await callAlice(`create_project_meta`, entry)
       tape.deepEqual(createResult.entry, entry)
       // READ
-      const fetchResult = await callAlice(`fetch_${entryType}`, null)
+      const fetchResult = await callAlice(`fetch_project_meta`, null)
       tape.deepEqual(fetchResult, createResult)
     }
   )
